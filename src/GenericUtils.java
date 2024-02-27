8ca98801ecc2ba131f2275deaedb77f2c024237d
Nico Furtner <nfurtner@student.tgm.ac.at>
2024-02-27 14:56:11 +0100

    Class and Methods addes

---
+++ src/GenericUtils.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+/**
+ * Class GenericUtils
+ * @author Nico Furtner
+ * @version 2024-27-02
+ */
+public class GenericUtils {
+    public static void reverse(List<?>[] array) {
+
+    }
+    public double sum(List<? extends Number>[] array) {
+
+        return 0.0;
+    }
+}
