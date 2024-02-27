e4bc5bcbc722ef2d108322c5dbc44652733e2847
Nico Furtner <nfurtner@student.tgm.ac.at>
2024-02-27 15:05:46 +0100

    Method reverse and tested

--- src/GenericUtils.java
+++ src/GenericUtils.java
@@ -6,8 +6,14 @@ import java.util.List;
  * @version 2024-27-02
  */
 public class GenericUtils {
-    public static void reverse(List<?>[] array) {
-
+    public static <T> void reverse(T[] array) {
+        int j = array.length - 1;
+        for(int i = 0; i < j; i++) {
+            T arr = array[i];
+            array[i] = array[j];
+            array[j] = arr;
+            j--;
+        }
     }
     public double sum(List<? extends Number>[] array) {
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        Integer[] integer = {1,2,3,4};
+        System.out.println("Array bevor dem reverse: " + Arrays.toString(integer));
+        GenericUtils.reverse(integer);
+        System.out.println("Array nach dem reverse: " + Arrays.toString(integer));
 
     }
 }
